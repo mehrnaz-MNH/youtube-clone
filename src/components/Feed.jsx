755674a4c401a3f53ada7b34416ea6dc342c4ac8
mehrnaz-MNH <mnh.mehrnaz@gmail.com>
2023-09-06 23:55:17 +1000

    started to implement sidebar

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -5,8 +5,21 @@ const Feed = () => {
   return (
     <Stack sx={{flexDirection:{ sx:"column", md : "row"}}}>
 
+      <Box sx={{ height:{sx:'auto' , md:'92vh'} ,
+      borderRight:'1px solid #3d3d3d' ,
+      px:{sx:0,md:2}}}>
+        SideBar
+        <Typography className="copyright"
+       variant="body2"
+       sx={{mt:1.5,color:'#fff'}}>
+            Copyright 2023 Mehrnaz Media
+      </Typography>
+       </Box>
+
+
     </Stack>
   )
+
 }
 
 export default Feed
